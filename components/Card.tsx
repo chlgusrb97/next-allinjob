2ed24f0a89c19f82908f7c2e5dee4006919f7f2f
nearworld <artemismars2@outlook.com>
2024-04-07 00:55:54 +0900

    feat: useQuery를 이용한 카드 데이터 렌더링

--- components/Card.tsx
+++ components/Card.tsx
@@ -3,17 +3,27 @@ import BookMark from "@/public/bookmark.svg";
 import BookMarkIndicator from "@/public/bookmark_indicator.svg";
 import Visibility from "@/public/visibility.svg";
 import Image from "next/image";
+import { CardType } from "./CardList";
 
 type Props = {
+  data: CardType;
   index: number;
 };
 
 export default function Card(props: Props) {
+  const { index, data } = props;
   const isSpecial = props.index < 4 ? "block" : "hidden";
 
   return (
-    <div key={props.index}>
-      <div className="relative aspect-square w-full rounded-lg bg-black-50">
+    <div key={index}>
+      <div className="relative aspect-square w-full overflow-hidden rounded-lg bg-black-50">
+        <Image
+          alt="image"
+          src={data.mainImage}
+          fill
+          sizes="(min-width: 808px) 50vw, 100vw"
+          priority
+        />
         <div className="absolute left-2 top-2 flex gap-2">
           <div
             className={cn(
@@ -24,7 +34,7 @@ export default function Card(props: Props) {
             special
           </div>
           <div className="rounded-md border-2 border-orange-500 bg-orange-50 px-2 py-1 text-orange-500">
-            d-day
+            {data.Dday}
           </div>
         </div>
         <div className="absolute bottom-2 right-2">
@@ -32,8 +42,8 @@ export default function Card(props: Props) {
         </div>
       </div>
       <div className="mt-2">
-        <h1 className="text-lg">제목 오는 곳</h1>
-        <p className="text-sm text-black-300">주최기관</p>
+        <h1 className="h-[40px] text-lg">{data.title}</h1>
+        <p className="text-sm text-black-300">{data.enterprise}</p>
         <div className="float-right flex gap-2 text-sm">
           <div className="flex gap-1">
             <Image
@@ -41,11 +51,11 @@ export default function Card(props: Props) {
               src={BookMarkIndicator}
               width={20}
             />
-            <span>10</span>
+            <span>{data.scrap}</span>
           </div>
           <div className="flex gap-1">
             <Image alt="visibility" src={Visibility} width={20} />
-            <span>10</span>
+            <span>{data.view}</span>
           </div>
         </div>
       </div>
--- components/CardList.tsx
+++ components/CardList.tsx
@@ -1,13 +1,39 @@
+import { useQuery } from "@tanstack/react-query";
 import Card from "./Card";
 
+export type CardType = {
+  id: string;
+  Dday: number;
+  scrap: number;
+  mainImage: string;
+  view: number;
+  title: string;
+  enterprise: string;
+};
+
 const CardList = () => {
-  return (
-    <div className="col-span-12 grid grid-cols-4 gap-[15px]">
-      {Array.from({ length: 12 }).map((_, index) => (
-        <Card key={index} index={index} />
-      ))}
-    </div>
-  );
+  const { data, isLoading } = useQuery({
+    queryKey: ["card"],
+    queryFn: async (): Promise<CardType[]> => {
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/crawling/main/outside`,
+      );
+      const data = await res.json();
+      console.log(data);
+      return data.data;
+    },
+  });
+
+  if (isLoading) return <div>loading...</div>;
+
+  if (data)
+    return (
+      <div className="col-span-12 grid grid-cols-4 gap-[15px]">
+        {data.map((item, index) => (
+          <Card key={item.id} index={index} data={item} />
+        ))}
+      </div>
+    );
 };
 
 export default CardList;
