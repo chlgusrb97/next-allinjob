0787f36b4ad7b198fce1bf40154cca06051e895b
nearworld <artemismars2@outlook.com>
2024-04-14 10:38:48 +0900

    feat: react query devtools 설치 및 queryClient 인스턴스 상태로 저장

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,11 +8,12 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
 import RootLayout from "@/components/layout/RootLayout";
-import React from "react";
-
-const queryClient = new QueryClient();
+import React, { useState } from "react";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(new QueryClient());
+
   let LayoutComponent = RootLayout;
 
   switch (pageProps.layout) {
@@ -34,6 +35,7 @@ export default function App({ Component, pageProps }: AppProps) {
             <Component {...pageProps} />
           </LayoutComponent>
         </HydrationBoundary>
+        <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </RecoilRoot>
   );
