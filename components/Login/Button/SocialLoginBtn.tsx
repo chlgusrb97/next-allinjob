b669b1166466c40a2bff3391fcf2f2653983c796
chlgusrb97 <ikolp34@gmail.com>
2024-04-15 23:29:55 +0900

    feat: 구글OAuth api

--- components/Login/Button/SocialLoginBtn.tsx
+++ components/Login/Button/SocialLoginBtn.tsx
@@ -6,8 +6,19 @@ type Props = {
 };
 
 export default function SocialLoginButton(props: Props) {
+  const googleLoginHandler = () => {
+    const googleOptions =
+      "toolbar=no,scrollbars=no,resizable=no,status=no,menubar=no,width=500,height=600,top=50,left=50";
+
+    window.open(
+      `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_GOOGLE_CALLBACK_URL}&response_type=token&scope=email`,
+      "_blank",
+      googleOptions,
+    );
+  };
+
   return (
-    <button>
+    <button onClick={googleLoginHandler}>
       <Image
         className="mb-3"
         src={props.imageSrc}
