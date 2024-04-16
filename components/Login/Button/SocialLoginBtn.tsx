4bc4915fac351a7f0e62ba2cc1756ef7efd69ebc
chlgusrb97 <ikolp34@gmail.com>
2024-04-17 03:41:43 +0900

    feat: 구글로그인 팝업 AccessToken 데이터 통신

--- components/Login/Button/SocialLoginBtn.tsx
+++ components/Login/Button/SocialLoginBtn.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 type Props = {
   socialName: "카카오" | "구글";
@@ -6,16 +7,50 @@ type Props = {
 };
 
 export default function SocialLoginButton(props: Props) {
+  const [popup, setPopup] = useState<Window | null>();
+
   const googleLoginHandler = () => {
     const googleOptions =
       "toolbar=no,scrollbars=no,resizable=no,status=no,menubar=no,width=500,height=600,top=50,left=50";
-
-    window.open(
+    const popup = window.open(
       `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_GOOGLE_CALLBACK_URL}&response_type=token&scope=email`,
       "_blank",
       googleOptions,
     );
+
+    setPopup(popup);
   };
+
+  useEffect(() => {
+    const hash = window.location.hash;
+    const parsedHash = new URLSearchParams(hash.substring(1));
+    const accessToken = parsedHash.get("access_token");
+
+    if (accessToken) {
+      window.opener.postMessage({ accessToken }, window.location.origin);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (!popup) return;
+
+    const getGoogleOAuthAccessToken = (e: MessageEvent) => {
+      if (e.origin !== window.location.origin) return;
+      const { accessToken } = e.data;
+      if (accessToken) {
+        popup.close();
+        setPopup(null);
+      }
+    };
+
+    window.addEventListener("message", getGoogleOAuthAccessToken, false);
+
+    return () => {
+      window.removeEventListener("message", getGoogleOAuthAccessToken);
+      popup?.close();
+      setPopup(null);
+    };
+  }, [popup]);
 
   return (
     <button onClick={googleLoginHandler}>
