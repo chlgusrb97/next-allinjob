cc274112ce5a9d8b3d8a5e7b9f0b144f77151ace
chlgusrb97 <ikolp34@gmail.com>
2024-04-17 04:56:20 +0900

    feat: 카카오 로그인 팝업 code 데이터 통신

--- components/Login/Button/SocialLoginBtn.tsx
+++ components/Login/Button/SocialLoginBtn.tsx
@@ -1,3 +1,4 @@
+import { useQuery } from "@tanstack/react-query";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 
@@ -13,11 +14,19 @@ export default function SocialLoginButton(props: Props) {
     const googleOptions =
       "toolbar=no,scrollbars=no,resizable=no,status=no,menubar=no,width=500,height=600,top=50,left=50";
     const popup = window.open(
-      `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_GOOGLE_CALLBACK_URL}&response_type=token&scope=email`,
+      `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_CALLBACK_URL}&response_type=token&scope=email`,
       "_blank",
       googleOptions,
     );
+    setPopup(popup);
+  };
 
+  const kakaoLoginHandler = () => {
+    const popup = window.open(
+      `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_KAKAO_REST_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_CALLBACK_URL}&response_type=code`,
+      "_blank",
+      "width=500,height=600,top=50,left=50",
+    );
     setPopup(popup);
   };
 
@@ -28,6 +37,13 @@ export default function SocialLoginButton(props: Props) {
 
     if (accessToken) {
       window.opener.postMessage({ accessToken }, window.location.origin);
+    }
+
+    const kakaoHref = window.location.href;
+    const code = new URL(kakaoHref).searchParams.get("code");
+
+    if (code) {
+      window.opener.postMessage({ code }, window.location.origin);
     }
   }, []);
 
@@ -43,17 +59,29 @@ export default function SocialLoginButton(props: Props) {
       }
     };
 
-    window.addEventListener("message", getGoogleOAuthAccessToken, false);
+    const getKakaoOAuthAccessToken = (e: MessageEvent) => {
+      if (e.origin !== window.location.origin) return;
+      const { code } = e.data;
+      if (code) {
+        popup.close();
+        setPopup(null);
+      }
+    };
+
+    window.addEventListener("message", getKakaoOAuthAccessToken, false);
 
     return () => {
-      window.removeEventListener("message", getGoogleOAuthAccessToken);
+      window.removeEventListener("message", () => {
+        if (props.socialName === "구글") getGoogleOAuthAccessToken;
+        if (props.socialName === "카카오") getKakaoOAuthAccessToken;
+      });
       popup?.close();
       setPopup(null);
     };
   }, [popup]);
 
   return (
-    <button onClick={googleLoginHandler}>
+    <button onClick={kakaoLoginHandler}>
       <Image
         className="mb-3"
         src={props.imageSrc}
