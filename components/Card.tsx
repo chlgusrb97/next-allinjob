0b7fcf461e6eed493cae80ef4feb585a35da3306
nearworld <artemismars2@outlook.com>
2024-04-17 17:46:26 +0900

    refactor: 카드 컴포넌트에서 Header, Footer 컴포넌트 생성

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,9 +1,9 @@
-import { cn } from "@/lib/utils";
 import { CardType } from "@/pages";
 import BookMark from "@/public/bookmark.svg";
-import BookMarkIndicator from "@/public/bookmark_indicator.svg";
-import Visibility from "@/public/visibility.svg";
+
 import Image from "next/image";
+import CardFooter from "./CardFooter";
+import CardHeader from "./CardHeader";
 
 type Props = {
   data: CardType;
@@ -12,53 +12,30 @@ type Props = {
 
 export default function Card(props: Props) {
   const { index, data } = props;
-  const isSpecial = props.index < 4 ? "block" : "hidden";
 
   return (
-    <div key={index}>
-      <div className="relative aspect-square w-full overflow-hidden rounded-lg bg-black-50">
-        <Image
-          alt="image"
-          src={data.mainImage}
-          fill
-          sizes="(min-width: 808px) 50vw, 100vw"
-          priority
-        />
-        <div className="absolute left-2 top-2 flex gap-2">
-          <div
-            className={cn(
-              isSpecial,
-              "rounded-md bg-orange-500 px-2 py-1 text-white",
-            )}
-          >
-            special
-          </div>
-          <div className="rounded-md border-2 border-orange-500 bg-orange-50 px-2 py-1 text-orange-500">
-            {data.Dday}
+    <div key={index} className="flex flex-col justify-between">
+      <div>
+        <div className="relative aspect-square w-full  overflow-hidden rounded-lg bg-black-50">
+          <Image
+            alt="image"
+            src={data.mainImage}
+            fill
+            sizes="(min-width: 808px) 50vw, 100vw"
+            priority
+          />
+          <CardHeader data={data} index={index} />
+          <div className="absolute bottom-2 right-2">
+            <Image alt="bookmark" src={BookMark} width={20} />
           </div>
         </div>
-        <div className="absolute bottom-2 right-2">
-          <Image alt="bookmark" src={BookMark} width={20} />
+
+        <div className="mt-2">
+          <h1 className="text-lg">{data.title}</h1>
         </div>
       </div>
-      <div className="mt-2">
-        <h1 className="h-[40px] text-lg">{data.title}</h1>
-        <p className="text-sm text-black-300">{data.enterprise}</p>
-        <div className="float-right flex gap-2 text-sm">
-          <div className="flex gap-1">
-            <Image
-              alt="bookmark_indicator"
-              src={BookMarkIndicator}
-              width={20}
-            />
-            <span>{data.scrap}</span>
-          </div>
-          <div className="flex gap-1">
-            <Image alt="visibility" src={Visibility} width={20} />
-            <span>{data.view}</span>
-          </div>
-        </div>
-      </div>
+
+      <CardFooter data={data} />
     </div>
   );
 }
