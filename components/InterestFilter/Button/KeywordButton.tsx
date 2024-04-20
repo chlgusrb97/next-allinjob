d1336e903d62c9e9ae31cd720ba36b51b0a3b856
chlgusrb97 <ikolp34@gmail.com>
2024-04-21 03:43:50 +0900

    design: 키워드버튼 컴포넌트 삭제버튼 조건부 렌더링

--- components/InterestFilter/Button/KeywordButton.tsx
+++ components/InterestFilter/Button/KeywordButton.tsx
@@ -1,11 +1,23 @@
+import Image from "next/image";
+
 type Props = {
   keyword: string;
+  isShowDelete?: boolean;
 };
 
-const KeywordButton = ({ keyword }: Props) => {
+const KeywordButton = ({ keyword, isShowDelete }: Props) => {
   return (
-    <button className="rounded-full border-2 border-background-primary bg-white px-[14px] py-[6px] font-medium text-black-300">
-      {keyword}
+    <button className="flex items-center rounded-full border-2 border-background-primary bg-white px-[14px] py-[6px] font-medium text-black-300">
+      #{keyword}
+      {isShowDelete && (
+        <Image
+          src="/delete.svg"
+          alt="삭제 아이콘"
+          width={20}
+          height={20}
+          className="ml-2"
+        />
+      )}
     </button>
   );
 };
