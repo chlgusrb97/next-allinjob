d61c5a62dc93fa494a570e88de90d299a06d230a
nearworld <artemismars2@outlook.com>
2024-04-24 15:10:03 +0900

    refactor: react-slick으로 구현한 캐러셀 코드는 Carousel 컴포넌트로 분리

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,7 @@
 import CardList, { getPosts } from "@/components/Card/CardList";
+import Carousel from "@/components/Carousel";
 import { QueryClient, dehydrate } from "@tanstack/react-query";
-import { ChevronLeft, ChevronRight } from "lucide-react";
 import { GetServerSideProps } from "next";
-import Image from "next/image";
-import { useCallback, useRef } from "react";
-import Slider from "react-slick";
 
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
@@ -42,60 +39,9 @@ export const getServerSideProps: GetServerSideProps = async () => {
 };
 
 export default function Home() {
-  const slickRef = useRef<Slider>(null);
-  const previous = useCallback(
-    () => slickRef?.current && slickRef.current.slickPrev(),
-    [],
-  );
-  const next = useCallback(
-    () => slickRef?.current && slickRef.current.slickNext(),
-    [],
-  );
-
-  var settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    dotsClass: "slick-dots",
-    arrows: false,
-    cursor: "pointer",
-  };
-
-  const images = [
-    "/carousel/lake.jpg",
-    "/carousel/lake.jpg",
-    "/carousel/lake.jpg",
-  ];
-
   return (
     <>
-      <div className="slider-container relative col-span-12">
-        <Slider
-          ref={slickRef}
-          {...settings}
-          dotsClass="slick-dots"
-          className="relative"
-        >
-          {images.map((image, index) => (
-            <div
-              key={index}
-              className="relative h-[400px] w-[1200px] bg-orange-400"
-            >
-              <Image fill src={image} alt="carousel image" />
-            </div>
-          ))}
-        </Slider>
-        <div onClick={previous} className="absolute top-1/2">
-          <ChevronLeft className="text-white" />
-        </div>
-        <div onClick={next} className="absolute right-0 top-1/2">
-          <ChevronRight className="text-white" />
-        </div>
-      </div>
+      <Carousel />
       <CardList />
     </>
   );
